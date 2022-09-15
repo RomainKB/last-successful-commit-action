1764cdb09831dff7c72167b28c2c7deef99210d6
Romain KB <romain.kuss-brieke@finalcad.com>
2022-09-15 15:01:40 +0200

    Remove logs

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const github = require("@actions/github");
 try {
   const octokit = github.getOctokit(core.getInput("github_token"));
   const owner = github.context.repo.owner
-  console.log("Owner " + owner);
   const repo = github.context.repo.repo
-  console.log("Repo " + repo);
-  console.log("workflow_id " + core.getInput("workflow_id"));
-  console.log("branch " + core.getInput("branch"));
-
   octokit.actions
     .listWorkflowRuns({
       owner,
@@ -19,7 +14,6 @@ try {
       branch: core.getInput("branch"),
     })
     .then((res) => {
-      console.log("Length " + res.data.workflow_runs.length);
       const lastSuccessCommitHash =
         res.data.workflow_runs.length > 0
           ? res.data.workflow_runs[0].head_commit.id
